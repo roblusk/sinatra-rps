79e7f4aea6e08aafa1824ad6e50423d7b6271f91
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-10 21:05:30 +0000

    Created each route and game logic

--- app.rb
+++ app.rb
@@ -1,9 +1,67 @@
 require "sinatra"
 require "sinatra/reloader"
+def game(our_choice)
+  they_chose = rand(1..3)
+  if they_chose == 1
+    they_chose = "rock"
+  elsif they_chose == 2
+    they_chose = "paper"
+  else
+    they_chose = "scissors"
+  end
 
+  response = "<h1>We played #{our_choice}!</h1>"
+  response += "<h2>They played #{they_chose}!</h2>"
+
+  if our_choice == they_chose
+    response += "<h3>We tied!</h3>"
+  elsif (our_choice == "rock" && they_chose == "scissors") || (our_choice == "paper" && they_chose == "rock") || (our_choice == "scissors" && they_chose == "paper")
+    response += "<h3>We won!</h3>"
+  else
+    response += "<h3>We lost!</h3>"
+  end
+
+  response
+end
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+def game(our_choice)
+  they_chose = rand(1..3)
+  if they_chose == 1
+    they_chose = "rock"
+  elsif they_chose == 2
+    they_chose = "paper"
+  else
+    they_chose = "scissors"
+  end
+
+  response = "<h1>We played #{our_choice}!</h1>"
+  response += "<h2>They played #{they_chose}!</h2>"
+
+  if our_choice == they_chose
+    response += "<h3>We tied!</h3>"
+  elsif (our_choice == "rock" && they_chose == "scissors") || (our_choice == "paper" && they_chose == "rock") || (our_choice == "scissors" && they_chose == "paper")
+    response += "<h3>We won!</h3>"
+  else
+    response += "<h3>We lost!</h3>"
+  end
+
+  response
+end
+
+get("/rock") do
+  @response = game("rock")
+  erb(:game)
+end
+
+get("/paper") do
+  @response = game("paper")
+  erb(:game)
+end
+
+get("/scissors") do
+  @response = game("scissors")
+  erb(:game)
 end
