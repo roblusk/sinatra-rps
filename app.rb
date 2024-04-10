7ae9838c01a5ffc8cd9adebdcadac7c9359a3620
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-10 21:14:50 +0000

    added game.erb

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+
 def game(our_choice)
   they_chose = rand(1..3)
   if they_chose == 1
@@ -11,14 +12,14 @@ def game(our_choice)
   end
 
   response = "<h1>We played #{our_choice}!</h1>"
-  response += "<h2>They played #{they_chose}!</h2>"
+  response += "<h1>They played #{they_chose}!</h1>"
 
   if our_choice == they_chose
-    response += "<h3>We tied!</h3>"
+    response += "<h1>We tied!</h1>"
   elsif (our_choice == "rock" && they_chose == "scissors") || (our_choice == "paper" && they_chose == "rock") || (our_choice == "scissors" && they_chose == "paper")
-    response += "<h3>We won!</h3>"
+    response += "<h1>We won!</h1>"
   else
-    response += "<h3>We lost!</h3>"
+    response += "<h1>We lost!</h1>"
   end
 
   response
@@ -27,41 +28,17 @@ get("/") do
   erb(:rules)
 end
 
-def game(our_choice)
-  they_chose = rand(1..3)
-  if they_chose == 1
-    they_chose = "rock"
-  elsif they_chose == 2
-    they_chose = "paper"
-  else
-    they_chose = "scissors"
-  end
-
-  response = "<h1>We played #{our_choice}!</h1>"
-  response += "<h2>They played #{they_chose}!</h2>"
-
-  if our_choice == they_chose
-    response += "<h3>We tied!</h3>"
-  elsif (our_choice == "rock" && they_chose == "scissors") || (our_choice == "paper" && they_chose == "rock") || (our_choice == "scissors" && they_chose == "paper")
-    response += "<h3>We won!</h3>"
-  else
-    response += "<h3>We lost!</h3>"
-  end
-
-  response
-end
-
 get("/rock") do
-  @response = game("rock")
+  @result = game("rock")
   erb(:game)
 end
 
 get("/paper") do
-  @response = game("paper")
+  game("paper")
   erb(:game)
 end
 
 get("/scissors") do
-  @response = game("scissors")
+  game("scissors")
   erb(:game)
 end
